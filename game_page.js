13d1c383446032baa518cce56faec1b20ee165f3
manvithsai <90837315+manvithsai@users.noreply.github.com>
2022-07-05 12:19:03 +0530

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -10,3 +10,26 @@ row= question_number+input_box+Check_button;
 document.getElementById("output").innerHTML=row;
 document.getElementById("num1").value="";
 document.getElementById("num2").value="";
+question_turn="player1";
+answer_turn="player2";
+function check(){
+    get_answer=document.getElementById("input_check_box").value;
+    if(get_answer==actual_answer){
+    if(answer_turn=="player1"){
+        update_player1_score=player1_score+1;
+        document.getElementById("player1_score").innerHTML=update_player1_score;
+    }
+    else{
+        update_player2_score=player2_score+1;
+        document.getElementById("player2_score").innerHTML=update_player2_score;
+    }
+    }
+        if(question_turn=="player1"){
+            question_turn="player2"
+            document.getElementById("player_question").innerHTML="Question turn-"+player2_name;
+        }
+        else{
+            question_turn="player1"
+            document.getElementById("player_question").innerHTML="Question turn-"+player1_name;
+        }
+}
