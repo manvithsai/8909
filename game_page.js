a7bb3de3aec8ac0a51001961514a28215ba2852d
manvithsai <90837315+manvithsai@users.noreply.github.com>
2022-07-09 16:22:57 +0530

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -2,14 +2,14 @@
 function send(){
     number1=document.getElementById("num1").value;
     number2=document.getElementById("num2").value;
-actual_answer=parseInt(number1)*parseInt(number2);}
+actual_answer=parseInt(number1)*parseInt(number2);
 question_number="<h4>"+number1+"X"+number2+"</h4>";
 input_box="<br>Answer:<input type='text' id='input_check_box'>"
 Check_button="<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
 row= question_number+input_box+Check_button;
 document.getElementById("output").innerHTML=row;
 document.getElementById("num1").value="";
-document.getElementById("num2").value="";
+document.getElementById("num2").value="";}
 question_turn="player1";
 answer_turn="player2";
 function check(){
